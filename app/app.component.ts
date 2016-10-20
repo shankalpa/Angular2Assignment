4e0810d1d027c1664bce9a04da0db85e932b36c8
ShankalpaChaudhary <shankalpa.chaudhary@gmail.com>
2016-10-19 20:44:02 -0500

    Assignment 1, first commit

---
+++ app/app.component.ts
@@ -0,0 +1,33 @@
+﻿import { Component } from '@angular/core';
+export class Product {
+    id: number;
+    name: string;
+}
+@Component({
+    selector: 'my-app',
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
+})
+export class AppComponent {
+    title = "Products";
+    products = PRODUCTS;
+    selectedProduct: Product;
+    onSelect(product: Product): void {
+        this.selectedProduct = product;
+    }
+
+}
+
+const PRODUCTS: Product[] = [
+    { id: 11, name: 'Milk' },
+    { id: 12, name: 'Bread' },
+    { id: 13, name: 'Cereal' },
+    { id: 14, name: 'Jam' },
+    { id: 15, name: 'Butter' },
+    { id: 16, name: 'Oatmeal' },
+    { id: 17, name: 'Eggs' },
+    { id: 18, name: 'Applie' },
+    { id: 19, name: 'Orange' },
+    { id: 20, name: 'Bacon' }
+];
+
